df7206dff58533719a862f41bb1687f8537d4131
Summer <652308259@qq.com>
2016-05-14 11:26:44 +0800

    补充上次未上传的小改动

--- admin/Lib/Action/QuestionToolAction.class.php
+++ admin/Lib/Action/QuestionToolAction.class.php
@@ -100,9 +100,10 @@ class QuestionToolAction extends QuestionBaseAction{
 	public function checkDir($root){
 		if(!file_exists($root)){
 			mkdir($root);
-			if(!chmod($root, 0777)){
-				$this->error(L('OPERATION_FAILURE'));
-			}
+            chmod($root, 0777);
+//			if(!chmod($root, 0777)){
+//				$this->error(L('OPERATION_FAILURE'));
+//			}
 		}
 	}
 
